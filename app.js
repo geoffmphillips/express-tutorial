49a29093401b03170496916ccc1205d93e16cd78
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-08-06 01:07:26 +0000

    Added code to app.js, removed template code from home.ejs and post.ejs

---
+++ app.js
@@ -0,0 +1,63 @@
+/* app.js */
+
+//require and instantiate express
+var ejs = require('ejs');
+var express = require('express');
+var app = express();
+
+// fake posts to simulate database
+
+var posts = [
+  {
+    id: 1,
+    author: 'John',
+    title: 'Templating with EJS',
+    body: 'Blog post number 1'
+  },
+  {
+    id: 2,
+    author: 'Drake',
+    title: 'Express: Starting from the Bottom',
+    body: 'Blog post number 2'
+  },
+  {
+    id: 3,
+    author: 'Emma',
+    title: 'Streams',
+    body: 'Blog post number 3'
+  },
+  {
+    id: 4,
+    author: 'Cody',
+    title: 'Events',
+    body: 'Blog post number 4'
+  }
+];
+
+// set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// blog home page
+app.get('/', function(req, res) {
+  //render 'home.ejs' with the list of posts
+  res.render('home', {posts: posts});
+});
+
+//blog post
+app.get('post/:id', function(req, res) {
+  //find the post in the 'posts' array
+  var post = posts.filter(function(post) {
+    return post.id == req.params.id;
+  })[0];
+
+  // render the 'post.ejs' template with the post content
+  res.render('post', {
+    author: post.author,
+    title: post.title,
+    body: post.body
+  });
+});
+
+app.listen(8080);
+
+console.log('listening on port 8080');
